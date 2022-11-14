81395845f691e212bb068a02a4461f3ba0657b9b
ruzs <eddie10913@gmail.com>
2022-11-14 10:35:40 +0800

    mysqli_connect 連線方式

--- index.php
+++ index.php
@@ -10,16 +10,18 @@
 <body>
   <h1>學生管理系統</h1>
   <?php
-  $dsn="mysql:host=localhost;charset=utf8;dbname=school";
+  // $dsn="mysql:host=localhost;charset=utf8;dbname=school";
+  $db=mysqli_connect('localhost','root','','school');
+  mysqli_set_charset($db,'utf8');
 
-
-  $pdo= new PDO($dsn,'root','');
+  // $pdo= new PDO($dsn,'root','');
   //固定語法 new PDO(  ,'帳號(預設為root)'.'密碼');
 
   $sql="SELECT * FROM `students` LIMIT 5";
+  $result=mysqli_query($db,$sql);
+  $rows= mysqli_fetch_all($result);
 
-
-  $rows=$pdo ->query($sql)->fetchAll(PDO::FETCH_NAMED);
+  // $rows=$pdo ->query($sql)->fetchAll(PDO::FETCH_NAMED);
   //query() 查詢 fetch調資料 All為全部   (FETCH_NAMED ASSOC)
 
   echo "<pre>";
