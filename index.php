ed90f2f36cbbee5e7cc62fbfde1e48d7d1de838e
ruzs <eddie10913@gmail.com>
2022-11-14 10:34:11 +0800

    PDO 連線方式

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>學生管理系統</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  <h1>學生管理系統</h1>
+  <?php
+  $dsn="mysql:host=localhost;charset=utf8;dbname=school";
+
+
+  $pdo= new PDO($dsn,'root','');
+  //固定語法 new PDO(  ,'帳號(預設為root)'.'密碼');
+
+  $sql="SELECT * FROM `students` LIMIT 5";
+
+
+  $rows=$pdo ->query($sql)->fetchAll(PDO::FETCH_NAMED);
+  //query() 查詢 fetch調資料 All為全部   (FETCH_NAMED ASSOC)
+
+  echo "<pre>";
+  print_r($rows);
+  echo "</pre>";
+  ?>
+</body>
+</html>
