8008048e3d061d0f7e4e461ff42de4d90f001dcd
ruzs <eddie10913@gmail.com>
2022-11-21 15:26:04 +0800

    建立教師註冊表單頁面,及後端接收註冊資料的reg_user.php檔案 & 完成新增資料到使用者資料表

---
+++ api/reg_user.php
@@ -0,0 +1,24 @@
+<?php
+$dsn="mysql:host=localhost;charset=utf8;dbname=school";
+$pdo= new PDO($dsn,'root','');
+
+$acc=trim($_POST['acc']);
+$pw=trim($_POST['pw']);
+$name=trim($_POST['name']);
+$email=trim($_POST['email']);
+$last_login=null;
+
+$sql="insert into `users`(`acc`,`pw`,`name`,`email`,`last_login`)
+values('$acc','$pw','$name','$email','$last_login')";
+echo "acc=>".$acc;
+echo "<br>";
+echo "pw=>".$pw;
+echo "<br>";
+echo "name=>".$name;
+echo "<br>";
+echo "email=>".$email;
+echo "<br>";
+$pdo->exec($sql);
+header("location:../login.php")
+
+?>
