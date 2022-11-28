127b7fd6ad09567072616e9983b82ed409e57dc5
ruzs <eddie10913@gmail.com>
2022-11-28 13:22:45 +0800

    建立儲存消息的api檔案，完成新增消息功能 &套入bootstrap框架

--- admin_center.php
+++ admin_center.php
@@ -13,6 +13,7 @@ include "base.php";
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>後台管理系統</title>
+  <?php include "./layouts/link_css.php";?>
   <link rel="stylesheet" href="style.css">
 </head>
 <body>
@@ -29,5 +30,6 @@ include "base.php";
         include "./back/main.php";
       }
     ?>
+    <?php include "./layouts/scripts.php";?>
 </body>
 </html>
