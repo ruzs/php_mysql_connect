386ccd1c0568313596fde1785ea1771e8ab051b6
ruzs <eddie10913@gmail.com>
2022-12-02 13:13:50 +0800

    使用不定參數來加強函式的使用彈性

---
+++ db.php
@@ -0,0 +1,43 @@
+<h1>資料庫常用自訂函式</h1>
+<h3>all()-存取指定條件的多筆資料</h3>
+<?php
+include_once "base.php";
+
+// $rows=all('students',['name'=>'宋時雨']);
+// dd($rows);
+// $rows=all('students',['id'=>'200']);
+// dd($rows);
+$rows=all('students',['dept'=>1,'graduate_at'=>1]," ORDER BY `id` desc");
+dd($rows);
+function dd($array){
+  echo "<pre>";
+  print_r($array);
+  echo "</pre>";
+}
+
+function all($table,...$args){
+  global $pdo;
+  $sql="select * from $table";
+
+  if (isset($args[0])) {
+    if (is_array($args[0])) {
+      //是陣列 [`acc`=>'mack' , `pw`=>'1234';]
+      //是陣列 [`product`=>'PC' , `price`=>'10000']
+
+      // $tmp='';
+      foreach ($args[0] as $key => $value) {
+        $tmp[]= "`$key`='$value'";
+      }
+      // $tmp=trim($tmp," && ");
+
+      $sql = $sql ." WHERE " . join(" && ",$tmp);
+    }else{
+      $sql=$sql . $args[0];
+    }
+  }
+
+  echo $sql;
+  return $pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+}
+
+?>
