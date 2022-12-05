a7e69dd704169f9384d24c9239c0a78356a3526f
ruzs <eddie10913@gmail.com>
2022-12-05 10:53:13 +0800

    新增調查列表功能 後端完成

--- api/survey_add.php
+++ api/survey_add.php
@@ -1,7 +1,30 @@
 <?php
-include_once "../db/base.php";
+include_once "../base.php";
 
+// $subject=$_POST['subjct'];
+// $type=1;
+// $vote=0;
+// $active=0;
+$subject=['subject'=>$_POST['subject'],
+          'type'=>1,
+          'vote'=>0,
+          'active'=>0];
 
+//使用insert 函式，寫入資料
+insert('survey_subject',$subject);
 
-
+$subject_id=find('survey_subject',['subject'=>$_POST['subject']])['id'];
+if(isset($_POST['opt'])){
+  foreach($_POST['opt'] as $option){
+    if($option!=''){
+        $tmp=['opt'=>$option,
+              'subject_id'=>$subject_id,
+              'vote'=>0];
+              dd($tmp);
+              insert('survey_options',$tmp);
+      }
+  }
+}
+//insert('survey_options');
+header("location:../admin_center.php?do=survey")
 ?>
