6bb14bec48a6eb32ac4f6d0891f8817d9d93c66f
ruzs <eddie10913@gmail.com>
2022-12-09 13:26:24 +0800

    增加log資料表，並在投票完成時增加一個log紀錄

--- api/survey_vote.php
+++ api/survey_vote.php
@@ -1,5 +1,6 @@
 <?php
 include_once "../base.php";
+
 if (isset($_POST['option'])) {
   $option_id=$_POST['option'];
   $option= find('survey_options',$option_id);
@@ -16,6 +17,26 @@ if (isset($_POST['option'])) {
   dd($subject);
   update("survey_subject",$subject,$subject['id']);
   update("survey_options",$option,$option['id']);
+  
+  if (!empty($_SERVER['HTTP_CLIENT_IP']))   //check ip from share internet
+    {
+      $ip=$_SERVER['HTTP_CLIENT_IP'];
+    }
+    elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR']))   //to check ip is pass from proxy
+    {
+      $ip=$_SERVER['HTTP_X_FORWARDED_FOR'];
+    }
+    else
+    {
+      $ip=$_SERVER['REMOTE_ADDR'];
+    }
+$log=[
+    'user'=>(isset($_SESSION['login']))?$_SESSION['login']['id']:0,
+    'ip'=>$ip,
+    'subject_id'=>$subject['id'],
+    'option_id'=>$option['id']
+];
+insert("survey_log",$log);
 
   to("../index.php?do=survey_result&id={$subject['id']}");
 }else{
